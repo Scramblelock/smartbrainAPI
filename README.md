d83b01f6f64928ec940605fac63ea1d3e20ee024
Marc <scramblelock@gmail.com>
2023-01-10 19:31:35 -0500

    fix Clarifai

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # smartbrainAPI
-A face-recognition web app with front-end in React and a back-end RESTful API server with Express. The project was deployed with Heroku and contains session management with authenticated routes. User information (username & hashed passwords) is stored securely in databases using PostgreSQL
+A face-recognition web app with front-end in React and a back-end RESTful API server with Express. The project was deployed with Vercel and contains session management with authenticated routes. User information (username & hashed passwords) is stored securely in databases using PostgreSQL
 
-https://smart-brain-marc.herokuapp.com/
+https://smartbrain-api.vercel.app/
--- server.js
+++ server.js
@@ -12,9 +12,11 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true, 
-  }
+    host: '127.0.0.1',
+    user: '',
+    password: '',
+    database: 'smart-brain',
+  },
 });
 
 db.select('*').from('users').then(data => {
@@ -23,16 +25,26 @@ db.select('*').from('users').then(data => {
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
-app.get('/', (req, res) => {res.send('it is working!');})
-app.post('/signin', (req, res) => {signin.handleSignin(db, bcrypt)(req, res)})
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db) })
-app.put('/image', (req, res) => {image.handleImage(req, res, db)}) 
-app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)}) 
+app.get('/', (req, res) => {
+  res.send(db.users);
+});
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.put('/image', (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post('/imageurl', (req, res) => {
+  image.handleApiCall(req, res);
+});
 
-app.listen(process.env.PORT || 3000, () => {
-	console.log(`app is running on port ${process.env.PORT}`);
-}) 
+app.listen(3000, () => {
+  console.log('app is running on port 3000');
+});
