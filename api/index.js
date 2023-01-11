644c7109f07af6f71d8d1f2f7faa4d2a5ca1a497
Marc <scramblelock@gmail.com>
2023-01-11 00:19:33 -0500

    cors & bcrypt fixes

--- api/index.js
+++ api/index.js
@@ -3,10 +3,10 @@ const bcrypt = require('bcrypt');
 const cors = require('cors');
 const knex = require('knex');
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const register = require('../controllers/register');
+const signin = require('../controllers/signin');
+const profile = require('../controllers/profile');
+const image = require('../controllers/image');
 
 const db = knex({
   client: 'pg',
@@ -21,6 +21,16 @@ const db = knex({
 const app = express();
 
 app.use(cors({ origin: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Origin, X-Requested-With, Accept, Authorization'
+  );
+  res.header('Access-Control-Expose-Headers', '*');
+  next();
+});
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
 app.get('/', (req, res) => {
