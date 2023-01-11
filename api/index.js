b7620b9728f9256078f0ede381812349aa8bb07f
Marc <scramblelock@gmail.com>
2023-01-11 18:07:56 -0500

    another cors fix

--- api/index.js
+++ api/index.js
@@ -20,17 +20,18 @@ const db = knex({
 
 const app = express();
 
-app.use(cors({ origin: true }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Origin, X-Requested-With, Accept, Authorization'
-  );
-  res.header('Access-Control-Expose-Headers', '*');
-  next();
-});
+const whitelist = ['https://smartbrain-ten.vercel.app/'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+app.use(cors(corsOptions));
+
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
 app.get('/', (req, res) => {
